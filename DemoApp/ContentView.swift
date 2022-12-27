84e513dfae6b83daeae7843fa6e0b7dd7b269b4a
Sengsathit <sengsathit@gmail.com>
2022-12-27 18:36:16 +0100

    First commit

---
+++ DemoApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  DemoApp
+//
+//  Created by Sengsathit SILALAK on 01/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("")
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by Sengsathit SILALAK on 01/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
